45fe3c49a94a850e1728f375db692bc462c2a3f2
Newton Manyisa <manyisanewton26@gmail.com>
2025-04-01 20:29:07 +0300

    done

--- server.js
+++ server.js
@@ -7,40 +7,36 @@ const path = require('path');
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+// Correct MongoDB Atlas URL (use regular connection string format)
+let mongodb_url = 'mongodb://hesbonangwenyi123:Test12345!@gallery-shard-00-00.wc344.mongodb.net:27017,darkroom-shard-00-01.wc344.mongodb.net:27017,darkroom-shard-00-02.wc344.mongodb.net:27017/?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority';
 let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
 
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// Connect to MongoDB
+mongoose.connect(`${mongodb_url}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database connected successfully');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB:', err);
+    });
 
 // Initializing the app
 const app = express();
 
-
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json())
-
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
+// Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
